8668a09447398c985b09914f1ebc3e4c0c506696
Mehak Mahajan <181397303+Mehak-mahajan@users.noreply.github.com>
2026-08-10 07:27:27 +0530

    Time: 0 ms (100%), Space: 46.7 MB (14.83%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -53,3 +53,20 @@ class Solution {
         
     }
 }
+
+// optimal approach 
+// original copy original copy 
+// 1 step insert copies copy = temp.nexxt right it is creatin a new node and my copy i.e 1' -> temp.next  2 because temp is at 1 
+// 1'-> 2 now mytemp will link to copy so temp.next = copy ie 1 -> 1'
+// now temp = temp.next it repats all process of inserting until loop finsishes 
+
+// 2nd step 
+// connect random pointers till now 1 -> 1' -> 2 -> 2'->3 -> 3"
+// ike 1 it points to 3 2 ppomnirts to 1 and 3 points 2 random pointers 
+
+// 1 -> 1 '-> 2 -> 2'-> 3 -> 3'
+// temp .random if temp = 1 then it is 3 and for copy if at 1 temp.random.next and then u can do temp = temp.nexxt until loops stops
+
+// 3rd step 
+// separate original and copy we have save the head.next 
+// copy.next= copy.next.next and temp.next = copy.next yes then moe temp = temp.next
