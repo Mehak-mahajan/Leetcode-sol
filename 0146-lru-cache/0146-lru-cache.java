1e77d2b692b48a405cd33dac2220e13e89e6fccf
Mehak Mahajan <181397303+Mehak-mahajan@users.noreply.github.com>
2026-09-14 12:17:20 +0530

    Time: 16 ms (68.98%), Space: 59 MB (60.05%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,97 @@
+class LRUCache {
+
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    int capacity;
+    HashMap<Integer, Node> map;
+    Node head;
+    Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+
+        map = new HashMap<>();
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void addNode(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        Node node = map.get(key);
+
+        // This key was just used → make it most recently used
+        removeNode(node);
+        addNode(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        // Key already exists
+        if (map.containsKey(key)) {
+
+            Node node = map.get(key);
+
+            node.value = value;
+
+            // It is now most recently used
+            removeNode(node);
+            addNode(node);
+
+        } else {
+
+            // Create new node
+            Node node = new Node(key, value);
+
+            map.put(key, node);
+            addNode(node);
+
+            // Cache exceeded capacity
+            if (map.size() > capacity) {
+
+                // Least recently used
+                Node lru = tail.prev;
+
+                removeNode(lru);
+                map.remove(lru.key);
+            }
+        }
+    }
+}
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
